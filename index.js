98ed630c8b6472c88a858e1a9c7d58e268fb8dfc
Charlene Johnson <charcodes24@gmail.com>
2021-04-26 18:14:37 -0700

    commit tree 91259f4e8bd4a64f6d18a3acb98e939903648631

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 54
+let num4 = 10
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 20);
+
+const mod = num3%num4;
+
+const max = Math.max(12, 3, 5, 6, 20, 9);
